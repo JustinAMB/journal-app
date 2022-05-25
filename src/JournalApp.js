a42f72adeb128af93c3caa0dd8f75cda06759e6f
JustinAMB <Justinmb@outlook.es>
2022-05-25 00:01:50 -0600

    se agrega el router y los estilos

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import AppRouter from './routers/AppRouter';
 
 const JournalApp = () => {
   return (
-    <h1>React</h1>
+    <AppRouter />
   )
 }
 
