bfe833a8e7f36395d58885c6dc93da43a1796a0e
JustinAMB <Justinmb@outlook.es>
2022-05-30 00:09:36 -0600

    se crea el componente de registro

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
 const RegisterScreen = () => {
   return (
-    <div>
-        <h1>RegisterScreen</h1>
-    </div>
+    <>
+    <h3 className="auth__title">Register</h3>
+      <form >
+        <input type="text" name="name" placeholder="name" className="auth__input mb-1"/>
+        <input type="email" name="email" placeholder="email" className="auth__input mb-1"/>
+        
+        <input type="password" name="password" placeholder="password" className="auth__input mb-1" />
+        <input type="password" name="password2" placeholder="confirmpassword" className="auth__input mb-1" />
+        <button type="submit" className="btn btn-primary btn-block mb-5">Create </button>
+        
+        
+        <Link to='/auth/login' className="link ">
+          Already registered?
+        </Link>
+      </form>
+    </>
   )
 }
 
