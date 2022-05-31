b30fd11ff29de49f6c7a407287b434d182f95a76
JustinAMB <Justinmb@outlook.es>
2022-05-31 00:24:22 -0600

    se crea el componente

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const JournalEntry = () => {
+  return (
+    <div className="journal__entry">
+        <div className="journal__entry-picture">
+            
+        </div>
+    </div>
+  )
+}
+
+export default JournalEntry
