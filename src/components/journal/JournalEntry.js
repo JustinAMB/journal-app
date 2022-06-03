7f9036acbc4e5595d6eb9c7a82b7346ccd9a3a5b
JustinAMB <Justinmb@outlook.es>
2022-06-02 23:22:16 -0600

    se crea la estructura del componente

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -3,8 +3,17 @@ import React from 'react'
 const JournalEntry = () => {
   return (
     <div className="journal__entry">
-        <div className="journal__entry-picture">
+        <div className="journal__entry-picture"
+        style={{
+           backgroundImage:url('https://static.videezy.com/system/resources/thumbnails/000/042/456/original/Sky-night-rotation-space-background.jpg'),
+           backgroundSize: 'cover',
+        }}>
             
+        </div>
+        <div className="journal__entry-body">
+          <p className="journal__entry-title">New day</p>
+          <p className="journal__entry-content">Lorem ipsum dolor sit amet consectetur adipisicing </p>
+
         </div>
     </div>
   )
