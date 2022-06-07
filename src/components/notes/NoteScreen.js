d460fa3feb874e7cdc44c40fb238e949b161ebe2
JustinAMB <Justinmb@outlook.es>
2022-06-06 23:22:25 -0600

    se crea la estructura html

---
+++ src/components/notes/NoteScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import  NotesAppBar from './NotesAppBar'
+const NoteScreen = () => {
+  return (
+    <div className="notes__main-content">
+      <NotesAppBar/>
+    </div>
+  )
+}
+
+export default NoteScreen
