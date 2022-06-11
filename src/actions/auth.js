f192601867ac95656b02f8498cd609d185e2f429
JustinAMB <Justinmb@outlook.es>
2022-06-10 23:32:14 -0600

    se crea la accion de login con google

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,18 @@
 import { types } from "../types/types"
+import {
+    firebase,
+    googleAuthProvider
+} from '../firebase/firebase-config';
 
+
+export const startGoogleLogin = () => {
+    return (dispatch) => {
+        firebase.auth().signInWithPopup(googleAuthProvider)
+            .then(userCard => {
+                console.log(userCard);
+            })
+    }
+}
 export const login = (uid, displayName) => ({
     type: types.login,
     payload: {
