9e6f3856533d024a5a86427599e6bba0804bf8fb
JustinAMB <Justinmb@outlook.es>
2022-06-14 20:12:34 -0600

    se inicializa el state

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,6 +1,12 @@
 import { types } from "../types/types";
 
-
+/*const initializeState = {
+    uid: 23456,
+    displayName: 'juan',
+    dir: {
+        b: 12
+    }
+}*/
 export const authReducer = (state = {}, action) => {
     switch (action.type) {
         case types.login:
