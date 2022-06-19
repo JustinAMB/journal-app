79c7930ba227b5f03a41ad0994d6b2e9053d10b9
JustinAMB <Justinmb@outlook.es>
2022-06-19 00:05:35 -0600

    se crea la action de registro con email y password

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,9 +1,9 @@
-import { types } from "../types/types"
+import { types } from "../types/types";
 import {
     firebase,
     googleAuthProvider
 } from '../firebase/firebase-config';
-
+import Swal from 'sweetalert2';
 
 export const startGoogleLogin = () => {
     return (dispatch) => {
@@ -23,3 +23,23 @@ export const login = (uid, displayName) => ({
 export const logout = () => ({
     type: types.logout
 })
+
+export const startRegisterWithEmailPasswordName = (email, password, name) => {
+    return (dispatch) => {
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(async({ user }) => {
+
+                await user.updateProfile({ displayName: name });
+
+                dispatch(
+                    login(user.uid, user.displayName)
+                );
+            })
+            .catch(e => {
+                console.log(e);
+                Swal.fire('Error', e.message, 'error');
+            })
+
+    }
+}
