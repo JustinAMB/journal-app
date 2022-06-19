3f6b7075a0dd5de5cbcf6d55788a62e145c6e395
JustinAMB <Justinmb@outlook.es>
2022-06-18 23:14:07 -0600

    implementa el useForm

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React from 'react';
 import {Link} from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
 const RegisterScreen = () => {
+  const [formValues,handleInputChange,]=useForm({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+  const {name,email,password,password2}=formValues;
   return (
     <>
     <h3 className="auth__title">Register</h3>
       <form >
-        <input type="text" name="name" placeholder="name" className="auth__input mb-1"/>
-        <input type="email" name="email" placeholder="email" className="auth__input mb-1"/>
+        <input type="text" name="name" placeholder="name" className="auth__input mb-1" value={ name }/>
+        <input type="email" name="email" placeholder="email" className="auth__input mb-1" value={ email }/>
         
-        <input type="password" name="password" placeholder="password" className="auth__input mb-1" />
-        <input type="password" name="password2" placeholder="confirmpassword" className="auth__input mb-1" />
+        <input type="password" name="password" placeholder="password" className="auth__input mb-1" value={ password }/>
+        <input type="password" name="password2" placeholder="confirmpassword" className="auth__input mb-1" value={ password2 } />
         <button type="submit" className="btn btn-primary btn-block mb-5">Create </button>
         
         
