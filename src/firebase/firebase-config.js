ec46d288cf4c84ead5f81e3470151bc700426c53
JustinAMB <Justinmb@outlook.es>
2022-06-19 00:26:47 -0600

    se corrige la inicializacion de authGoogle

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
-const googleAuthProvider = new firebase.auth.GoogleAuthProvider.GoogleAuthProvider();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 
 export {
